d63dfbc0971d8666ef99fc5c363f658af5dacb3d
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-01-28 17:59:19 -0500

    stretch done

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,32 @@
+const startTime = Date.now()
+let elapsed;
+const sTens = document.getElementById('secondTens')
+const sOnes = document.getElementById('secondOnes')
+const msHunds = document.getElementById('msHundreds')
+const msTens = document.getElementById('msTens')
+
+let done = false
+
+const interval = setInterval(runClock, 10);
+
+function runClock(){
+    elapsed = new Date(Date.now() - startTime);
+      
+    //seconds  
+    var secs = elapsed.getSeconds();
+    // hundredths
+    var ms = Math.floor(elapsed.getMilliseconds()/10);
+
+    sTens.textContent = Math.floor(secs/10)
+    sOnes.textContent = secs%10
+    msHunds.textContent = Math.floor(ms/10)
+    msTens.textContent = ms%10
+
+    if(secs === 10){
+        stop()
+    }
+}
+
+function stop() {
+    clearInterval(interval);
+  }
